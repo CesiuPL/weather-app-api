49f9633054e9a98b7061221cfd03a7825b97bd9f
Przemek Cesarz <przemek.cesiu@gmail.com>
2023-06-12 01:36:56 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city/',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: '908427c7aamshee8f3f991a0d48fp1e2892jsn29f9078f47ab'
+
+};
